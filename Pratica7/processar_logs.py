1e5485fadc623881bc056185d214a5c330154b83
Christopher <christophercruz080@gmail.com>
2025-09-03 09:53:25 -0300

    Pratica 7

---
+++ Pratica7/processar_logs.py
@@ -0,0 +1,21 @@
+"""
+Leia um arquivo que contenha dados de log de treinamento de modelos de Machine Learning.
+Calcule a média e o desvio padrão do tempo de exercução constantes.
+"""
+
+import pandas as pd
+
+def processar_logs_treinamento(nome_arquivo):
+    try:
+        df = pd.read_csv(nome_arquivo)
+        media_tempo = df['tempo_execucao'].mean()
+        desvio_padrao = df['tempo_execucao'].std()
+        print(f"Média do tempo de execução: {media_tempo:.2f} segundos")
+        print(f"DP tempo de execução: {desvio_padrao:.2f} segundos")
+    except FileNotFoundError:
+        print("Arquivo não encontradoo")
+    except Exception as e:
+        print(f"Errp {e} ao processar o arquivo")
+
+nome_arquivo = input("Digite o nome do arquivo")
+processar_logs_treinamento(nome_arquivo)
